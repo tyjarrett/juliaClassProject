efc7ef06380929b6d06d0e0704ba8c7f4e309ab4
Konstantine Partalas <kpartal1@trinity.edu>
2024-05-05 20:47:00 -0500

    added some visualizations

--- main.jl
+++ main.jl
@@ -1,5 +1,7 @@
 import Pkg
 Pkg.add("Plots")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
 
 using Plots, CSV, DataFrames
 
@@ -11,12 +13,8 @@ using Plots, CSV, DataFrames
 #PlotlyJS and Makie
 
 function openDataset()
-       
     DataFrame(CSV.File("bitcoin_2017_to_2023.csv"))
-
 end
-
-df = openDataset()
 
 #esseantially just a bunch of functions to graph diffrent parts of the data
 #run addOpenColor then graphOpenByDayWithColor to get color visuals
@@ -52,44 +50,6 @@ function addOpenColor(df)
     insertcols!(df, 8, :Color => output)
 end
 
-function graphOpen(df, hours)
-    start = nrow(df)
-    plot(df[start:-1:start-hours, 1], df[start:-1:start-hours, 2])
-end
-
-function graphOpen(df, start, hours)
-    start = nrow(df) - start
-    plot(df[start:-1:start-hours, 1], df[start:-1:start-hours, 2])
-end
-
-function graphOpenTime(df)
-    start = nrow(df)
-    plot(map(x -> Date(x), df[start:-1:start-100, 9]), df[start:-1:start-100, 2])
-end
-
-function graphOpen(df)
-    start = nrow(df)
-    plot(df[start:-1:1, 1], df[start:-1:1, 2])
-end
-
-function  graphOpenByHour(df, hours)
-
-    plot(df[1:60:hours*60, 1], df[1:60:hours*60, 2])
-    
-end
-
-function  graphOpenByDay(df, days)
-
-    plot(df[1:24*60:days*24*60, 1], df[1:24*60:days*24*60, 2])
-    
-end
-
-function  graphOpenByDay(df, days, start)
-
-    plot(df[start*24*60:24*60:days*24*60 + start*24*60, 1], df[start*24*60:24*60:days*24*60 + start*24*60, 2])
-    
-end
-
 function printColumn(df)
     output = [:green]
     start = nrow(df)
@@ -107,5 +67,59 @@ function  graphOpenByDayWithColor(df, days)
     ref = true
     markercolor = [ :green
                     :red ]
-    plot(df[1:days*24, 1], df[1:days*24, 2], color = df[1:days*24, 9])
+    Plots.plot(df[1:days*24, 1], df[1:days*24, 2], color = df[1:days*24, 9])
 end
+
+function histogramOfPrices(df)
+    Plots.histogram(df[:, 2], bins=20, xlabel="Price (USD)", ylabel="Frequency", title="Histogram of Bitcoin Prices", fill=true)
+end
+
+Pkg.add("PlotlyJS")
+using PlotlyJS
+Pkg.add("StatsBase")
+using StatsBase
+
+function colorBar(df)
+    cm = StatsBase.countmap(df.Color)
+    data = PlotlyJS.bar(;x = collect(keys(cm)), y = collect(values(cm)))
+    PlotlyJS.plot(data)
+end
+
+function OHLCByDays(df, days)
+    PlotlyJS.plot(PlotlyJS.ohlc(open = df[1:days*24, "open"], high = df[1:days*24, "high"], low = df[1:days*24, "low"], close = df[1:days*24, "close"]))
+end
+
+function candlestickByDays(df, days)
+    PlotlyJS.plot(PlotlyJS.candlestick(open = df[1:days*24, "open"], high = df[1:days*24, "high"], low = df[1:days*24, "low"], close = df[1:days*24, "close"]))
+end
+
+function heatmapHighToLowByVolumeByDays(df, days)
+    PlotlyJS.plot(PlotlyJS.heatmap(x = df[1:days*24, "high"], y = df[1:days*24, "low"], z = df[1:days*24, "volume"]))
+end
+
+function areaOfNumTradesToVolumeByDays(df, days)
+    s1 = PlotlyJS.scatter(; x = df[1:days*24, "timestamp"], y = df[1:days*24, "number_of_trades"], fill = "tonexty", name = "Number of Trades")
+    s2 = PlotlyJS.scatter(; x = df[1:days*24, "timestamp"], y = df[1:days*24, "volume"], fill = "tozeroy", name = "Volume")
+    PlotlyJS.plot([s1, s2], Layout(yaxis_range=[0, 1000]))
+end
+
+function boxSomeVars(df, days)
+    t1 = PlotlyJS.box(; y = df[1:days*24, "open"], name = "Open")
+    t2 = PlotlyJS.box(; y = df[1:days*24, "high"], name = "High")
+    t3 = PlotlyJS.box(; y = df[1:days*24, "low"], name = "Low")
+    t4 = PlotlyJS.box(; y = df[1:days*24, "close"], name = "Close")
+    # t5 = PlotlyJS.box(; y = df[1:days*24, "volume"], name = "Volume")
+    PlotlyJS.plot([t1, t2, t3, t4])
+end
+
+# main
+df = openDataset()
+addOpenColor(df)
+display(graphOpenByDayWithColor(df, 10))
+display(histogramOfPrices(df))
+display(colorBar(df))
+display(OHLCByDays(df, 1))
+display(candlestickByDays(df, 1))
+display(heatmapHighToLowByVolumeByDays(df, 1))
+display(areaOfNumTradesToVolumeByDays(df, 100))
+display(boxSomeVars(df, 100))
